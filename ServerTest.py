07da1894b4787819daca0827b2196af44c8c3b62
Eli Vermeulen <eli.vermeulen@student.ap.be>
2023-10-27 10:26:10 +0200

    Add drop menu

--- ServerTest.py
+++ ServerTest.py
@@ -1,15 +1,27 @@
-def DropMenu():
+import sys
+
+
+def DropMenu() -> int:
     print("Maak een keuze uit de volgende opties")
     print("1: Server toevoegen")
     print("2: Server verwijderen")
     print("3: Server lijst tonen")
-    ant = int(input())
+    print("4: Exit")
+    return int(input())
 
 
 while True:
     try:
-        DropMenu()
+        keuze = DropMenu()
+        match keuze:
+            case 1:
+                print("Server toevoegen")
+            case 2:
+                print("Server verwijderen")
+            case 3:
+                print("lijst tonen")
+            case 4:
+                sys.exit(0)
+
     except ValueError:
         print("Foute waarde")
-
-
